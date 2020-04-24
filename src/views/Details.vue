308f13a1f3a0410805cb1d92d42c485d9bb79999
marcospss <marcospss@gmail.com>
2020-04-24 20:17:48 -0300

    Estrutura projeto

---
+++ src/views/Details.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="details">
+    <h1>Details</h1>
+  </div>
+</template>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="not-found">
+    <h1>NOT FOUND PAGE</h1>
+  </div>
+</template>
